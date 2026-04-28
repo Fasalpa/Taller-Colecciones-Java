9575a35c7a8d070cb36fa120299d9a3cfbb26f69
fasalpa <psfr97@gmail.com>
2026-04-28 15:27:07 -0500

    Agregando Map y Set

--- src/HashMapT.java
+++ src/HashMapT.java
@@ -2,7 +2,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapT {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        String separador = "************************";
 
         Map<String, Double> empleados = new HashMap<String, Double>();
 
@@ -11,6 +13,21 @@ public class HashMapT {
         empleados.put("Cesar", 700.25);
 
 
+        for (Map.Entry<String, Double> entry : empleados.entrySet()) {
+            System.out.println("Empleado: " + entry.getKey() + " - Salario: " + entry.getValue());
+        }
+        empleados.put("Claudia", 3000.5);
+        System.out.println(separador +"\nCorrección de salario - Empleado: Claudia - Salario: " + empleados.get("Claudia"));
+
+        double total = 0;
+
+        for (double valor : empleados.values()) {
+            total = total + valor;
+        }
+
+        double promedio = total/ empleados.size();
+
+        System.out.println(separador +"\nPromedio de salarios: " + promedio);
 
     }
 }
